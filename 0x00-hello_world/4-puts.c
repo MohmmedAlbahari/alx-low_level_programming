e5a42ce6299377340862e76dad1556a05e034030
MohmmedAlbahari <mohmmedalbahari@gmail.com>
2022-12-08 10:15:01 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-/*
+/**
  * Print line using puts
- * Always Return(0)
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Return: 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
 
