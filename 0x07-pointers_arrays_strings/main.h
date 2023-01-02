675bea1bb2df073150a6f94194353d6e08bf3435
MohmmedAlbahari <mohmmedalbahari@gmail.com>
2023-01-02 12:00:04 -0800

    all task

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,9 +11,9 @@
 #include <stddef.h>
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
 void *_memcpy(void *dest, const void *src, size_t n);
-char *_strchr(char *s, char c);
+char *_strchr(void *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
