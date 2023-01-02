cea7daba2ba7551f075bc933d01297f1fac31efd
MohmmedAlbahari <mohmmedalbahari@gmail.com>
2023-01-02 12:03:17 -0800

    all task

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,8 +12,8 @@
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-void *_memcpy(void *dest, const void *src, size_t n);
-char *_strchr(void *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
