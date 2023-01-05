7ac788f7529645988dafe7ff4c8f535eb2d7e1f9
MohmmedAlbahari <mohmmedalbahari@gmail.com>
2023-01-04 22:20:42 -0800

    fix betty style

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,8 +4,8 @@
  * *_strcat - concatenates @src to @dest
  * @src: the source string to append to @dest
  * @dest: the destiation string to be concatenated upon
- * Return:pointer to the resulting string
-j*/
+ * Return:pointer to the resulting stringj
+ */
 
 char *_strcat(char *dest, char *src)
 {
